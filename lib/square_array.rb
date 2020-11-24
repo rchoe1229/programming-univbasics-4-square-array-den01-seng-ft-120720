84878221027232b7a4a9194edc9a4180b45778ed
Ryan Choe <ryan.choe@outlook.com>
2020-11-24 21:33:43 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -1,3 +1,9 @@
 def square_array(array)
-  # your code here
+  counter = 0 
+  new_numbers = []
+  
+  while counter < array.length do
+    new_numbers.push(array[counter] ** array[counter])
+  end
+  new_numbers
 end
